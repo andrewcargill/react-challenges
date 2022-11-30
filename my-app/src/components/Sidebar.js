60208c375c452966b00e9ef69a6f0d2e06352ebe
andrewcargill <andycargill01@aol.com>
2022-11-30 13:07:11 +0000

    sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import css from "./css/Sidebar.module.css";
+
+function Sidebar() {
+    return (
+        <div className={css.sidebar}>
+            <a href="">My Photos</a>
+            <a href="">My Illustrations</a>
+            <a href="">My Paintings</a>
+        </div>
+    )
+}
+export default Sidebar
