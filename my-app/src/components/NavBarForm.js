13bdc38ff23669bccd413a8bb5b84ff7319878c3
andrewcargill <andycargill01@aol.com>
2022-12-02 08:16:15 +0000

    working sign in form

--- my-app/src/components/NavBarForm.js
+++ my-app/src/components/NavBarForm.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react'
+import NavBarChild from './NavBarChild'
 
 export class NavBarForm extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-         isLoggedIn: true
+         isLoggedIn: false
       }
     }
+
+    handleClick() {
+      this.setState((prevState, prevProps) => {
+          console.log('prev state:', prevState.isLoggedIn)
+          
+          return {
+              isLoggedIn: prevState.isLoggedIn === false ? true : false,
+          }
+      })
+  }
 
   render() {
     return (
       <div className='css.NavBar'>
         <h1>My Gallery</h1>
-        <button></button>
+        <button onClick={()=> this.handleClick()}>Login</button>
+        <form>{this.state.isLoggedIn ? (
+                <div>
+                <label htmlFor="">Username: 
+                <input type="text" />
+                </label>
+
+                <label htmlFor="">Password: 
+                <input type="text" />
+                </label>
+                <button onClick={()=> this.handleClick()}>Submit</button>
+                </div>
+            ): (<p>Not signed in</p> )}</form>
       </div>
     )
   }
