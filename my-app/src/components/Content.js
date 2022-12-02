f333282b8559790d876c91f4d9060c8b0d3938b4
andrewcargill <andycargill01@aol.com>
2022-12-02 10:40:23 +0000

    rendering posts via a child

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from './PostItem';
+
+export class Content extends Component {
+
+//Render via PostItem.js    
+  render() {
+    return (
+      <div>
+        <div className={css.Content}>
+            <h1>My Photos</h1>
+        </div>
+        <div className={css.SearchResults}>
+        <PostItem savedPosts={savedPosts}/>
+        </div>
+      </div>
+    )
+  }
+
+//Render within the function  
+//   render() {
+//     return (
+//       <div>
+//         <div className={css.Content}>
+//             <h1>My Photos</h1>
+//         </div>
+//         <div className={css.SearchResults}>
+//         {savedPosts.map( post => {
+//             return <div className={css.SearchItem}>
+//                 <p>{post.title}</p>
+//                 <p>{post.name}</p>
+//                 <img src={post.image} alt="" />
+//                 <p>{post.description}</p>
+//             </div>
+//     })}
+//         </div>
+//       </div>
+//     )
+//   }
+}
+
+export default Content
--- my-app/src/components/NavBarForm.js
+++ my-app/src/components/NavBarForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import NavBarChild from './NavBarChild'
+import css from './css/NavBarForm.module.css'
 
 export class NavBarForm extends Component {
     constructor(props) {
@@ -11,7 +11,7 @@ export class NavBarForm extends Component {
     }
 
     handleClick() {
-      this.setState((prevState, prevProps) => {
+      this.setState((prevState) => {
           console.log('prev state:', prevState.isLoggedIn)
           
           return {
@@ -22,7 +22,7 @@ export class NavBarForm extends Component {
 
   render() {
     return (
-      <div className='css.NavBar'>
+      <div className={css.NavBar}>
         <h1>My Gallery</h1>
         <button onClick={()=> this.handleClick()}>Login</button>
         <form>{this.state.isLoggedIn ? (
@@ -36,7 +36,7 @@ export class NavBarForm extends Component {
                 </label>
                 <button onClick={()=> this.handleClick()}>Submit</button>
                 </div>
-            ): (<p>Not signed in</p> )}</form>
+            ): (<p></p> )}</form>
       </div>
     )
   }
