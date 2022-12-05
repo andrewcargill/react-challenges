27e2241fe4d134bf4bd10655cd1ed524f1383745
andrewcargill <andycargill01@aol.com>
2022-12-05 08:34:10 +0000

    using an API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,15 @@ import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
 import ContentHooks from './components/ContentHooks';
+import ContentAPI from "./components/ContentAPI";
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
       <NavBarForm />
-      <ContentHooks />
+      {/* <ContentHooks /> */}
+      <ContentAPI />
     </div>
   );
 }
